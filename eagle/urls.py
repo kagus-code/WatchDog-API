c4159790482e7a51d469c54f6ca3617b352146d1
Eston Kagwima <ekagwima745@gmail.com>
2021-06-06 01:22:14 +0300

    create views for business and posts

--- eagle/urls.py
+++ eagle/urls.py
@@ -6,10 +6,21 @@ from . import views
 
 
 urlpatterns = [
-
+#neighbourhood urls
   re_path(r'hood-get/(?P<pk>[0-9]+)/$',views.SingleHood.as_view(),name="single-hood"),
   re_path(r'^hood-post/$', views.NeighbourhoodApiView.as_view(),name="post-hood"),
   re_path(r'^hood-search/', views.SearchHood.as_view(), name='search_hood'),
+#business urls
+  re_path(r'business-get/(?P<pk>[0-9]+)/$',views.SingleBusiness.as_view(),name="single-business"),
+  re_path(r'^business-post/$', views.BusinessApiView.as_view(),name="post-business"),
+  re_path(r'^business-search/', views.SearchBusiness.as_view(), name='search_business'),
+  re_path(r'^business-hood/(?P<hood>\w+)/$', views.BusinessSortAPIView.as_view(), name='business_hood'),
+#post urls
+  
+  re_path(r'post-get/(?P<pk>[0-9]+)/$',views.SinglePost.as_view(),name="single-post"),
+  re_path(r'^post-post/$', views.PostApiView.as_view(),name="post-post"),
+  re_path(r'^post-search/', views.SearchPost.as_view(), name='search_post'),
+  re_path(r'^post-hood/(?P<hood>\w+)/$', views.PostSortAPIView.as_view(), name='post_hood'),
 
 
 ]
--- eagle/views.py
+++ eagle/views.py
@@ -10,8 +10,8 @@ from rest_framework import generics
 # api imports 
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import  NeighbourHood
-from .serializer import NeighbourhoodSerializer
+from .models import  NeighbourHood,Business,Post
+from .serializer import NeighbourhoodSerializer,BusinessSerializer,PostSerializer
 from rest_framework import permissions, status
 from rest_framework import filters
 
@@ -73,3 +73,127 @@ class SearchHood(generics.ListCreateAPIView):
 
 
 
+
+
+class BusinessApiView(APIView):
+  serializer_class = BusinessSerializer
+  def get(self, request, format=  None):
+    all_businesss = Business.objects.all()
+    serializers = BusinessSerializer(all_businesss,many=True)
+    return Response (serializers.data)
+  def post(self,request):
+    serializer =self.serializer_class(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+      serializer.save()
+      business_data =serializer.data
+      response={
+        "data":{
+            "business":dict(business_data),
+            "status":"success",
+            "message":"business added successfully",
+        }
+      }
+      return Response(response, status=status.HTTP_201_CREATED)
+    else:
+      return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class BusinessSortAPIView(APIView):
+  def get(self,request,hood):
+    hood=self.kwargs.get('hood')
+    business = Business.objects.filter(neighbourhood=hood)
+    serializers= b=BusinessSerializer(business, many = True)
+    return Response(serializers.data)
+
+class SingleBusiness(APIView):
+  def get_business(self,pk):
+    try:
+      return Business.objects.get(pk=pk)
+    except Business.DoesNotExist:
+      return Http404
+
+  def get(self,request,pk,format=None):
+    business =self.get_business(pk)
+    serializers = BusinessSerializer(business)
+    return Response(serializers.data)    
+  
+  def put(self,request,pk,format=None):
+    business = self.get_business(pk)
+    serializers = BusinessSerializer(business , request.data)
+    if serializers.is_valid():
+      serializers.save()
+      return Response(serializers.data)
+    else:
+      return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)  
+  
+  def delete(self, request, pk, format=None):
+    business = self.get_business(pk)
+    business.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)  
+class SearchBusiness(generics.ListCreateAPIView):
+  search_fields =['name']
+  filter_backends = (filters.SearchFilter,)
+  queryset = Business.objects.all()
+  serializer_class = BusinessSerializer
+
+
+
+
+class PostApiView(APIView):
+  serializer_class = PostSerializer
+  def get(self, request, format=  None):
+    all_posts = Post.objects.all()
+    serializers = PostSerializer(all_posts,many=True)
+    return Response (serializers.data)
+  def post(self,request):
+    serializer =self.serializer_class(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+      serializer.save()
+      business_data =serializer.data
+      response={
+        "data":{
+            "business":dict(business_data),
+            "status":"success",
+            "message":"post added successfully",
+        }
+      }
+      return Response(response, status=status.HTTP_201_CREATED)
+    else:
+      return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class SinglePost(APIView):
+  def get_post(self,pk):
+    try:
+      return Post.objects.get(pk=pk)
+    except Post.DoesNotExist:
+      return Http404
+
+  def get(self,request,pk,format=None):
+    post =self.get_post(pk)
+    serializers = PostSerializer(post)
+    return Response(serializers.data)    
+  
+  def put(self,request,pk,format=None):
+    post = self.get_post(pk)
+    serializers = PostSerializer(post , request.data)
+    if serializers.is_valid():
+      serializers.save()
+      return Response(serializers.data)
+    else:
+      return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)  
+  
+  def delete(self, request, pk, format=None):
+    post = self.get_post(pk)
+    post.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)  
+class SearchPost(generics.ListCreateAPIView):
+  search_fields =['post']
+  filter_backends = (filters.SearchFilter,)
+  queryset = Post.objects.all()
+  serializer_class = PostSerializer
+
+class PostSortAPIView(APIView):
+  def get(self,request,hood):
+    hood=self.kwargs.get('hood')
+    post = Post.objects.filter(hood=hood)
+    serializers= PostSerializer(post, many = True)
+    return Response(serializers.data)
