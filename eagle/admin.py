ca2e9d7154e23f0d3cd61602d8abd150204aeff7
Eston Kagwima <ekagwima745@gmail.com>
2021-06-05 17:00:55 +0300

    adding models to admin

--- eagle/admin.py
+++ eagle/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import NeighbourHood,User
+from .models import NeighbourHood,User,Business,Post,Profile
 
 admin.site.register(NeighbourHood)
 admin.site.register(User)
-# admin.site.register(Review)
+admin.site.register(Business)
+admin.site.register(Post)
+admin.site.register(Profile)
