e49fa3412770d75ad64b97c77a9db0189300adaa
Eston Kagwima <ekagwima745@gmail.com>
2021-06-06 17:10:23 +0300

    installing cors to connect to angular

--- watchdogs/settings.py
+++ watchdogs/settings.py
@@ -69,12 +69,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'eagle',
     'rest_framework',
-    'cloudinary'
+    'cloudinary',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -167,3 +169,8 @@ REST_FRAMEWORK = {
 }
 
 # LOGIN_REDIRECT_URL = 'post-hood'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200/"
+    
+]
